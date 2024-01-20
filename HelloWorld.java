4c8d802395f348afc061072b421e40b095c0edcd
Claudia Lovo <cplovo@loyola.edu>
2024-01-19 22:43:34 -0500

    Lab0 submission

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,11 @@
 class HelloWorld {
     public static void main(String[] args) {
 
-        System.out.println("Hello, World!");
-        
+
+        System.out.println ("             ___    ___   _           ");
+        System.out.println("            |      |   |  |            ");
+        System.out.println("            |      |__ /  |            ");
+        System.out.println("             ___   |      |___         ");
+
     }
 }
